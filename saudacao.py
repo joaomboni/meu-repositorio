fbf8720ad8b7a16d472ceee8efa116fc72b2465f
João Felipe Miranda Bonifácio <74162506+joaomboni@users.noreply.github.com>
2021-01-06 21:25:40 -0300

    Add files via upload

---
+++ saudacao.py
@@ -0,0 +1,12 @@
+turno = str(input('Qual turno você estuda ? M - matituno, V-vespertino ou N-noturno ' ))
+if turno == 'M':
+    print('Bom dia')
+elif turno == 'V':
+    print('Boa tarde')
+elif turno == 'N':
+    print('Boa noite')
+else:
+    print('Valor invalido')
+    
+
+
