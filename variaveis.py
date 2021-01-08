7c38cd1a120884dd925a16950d33ec1b7a68697c
João Felipe Miranda Bonifácio <74162506+joaomboni@users.noreply.github.com>
2021-01-08 10:46:35 -0300

    identificando variáveis
    “Declaro para o senhor Gonçalves Dias que o senhor Humberto Delgado
    esteve presente no evento SecurityCup e gastou o valor de R$ 30,00 com a
    entrada.”
    
    Identifique o que pode ser variável no texto acima e monte um projeto no
    Python, em um novo arquivo chamado Variaveis2. No final, deverá exibir o texto
    completo com o valor das variáveis. Agora, pare, siga para o PyCharm e tente
    resolver!

---
+++ variaveis.py
@@ -0,0 +1,6 @@
+responsavel = input('Digite o nome do responsavel:\n ')
+funcionario = input('Digite o nome do funcionario:\n ')
+evento = input('Digite o nome do evento:\n ')
+valor = input('Digite o valor gasto:\n ')
+print('Declaro para o senhor',responsavel,'que o senhor',funcionario,
+      'esteve presente no evento',evento,'e gastou o valor de R$',valor,'com entrada.' )
